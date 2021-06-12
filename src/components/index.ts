3706422698def35676f573f1d9412082c954a355
Juan Gamonal H <juangamonalh@gmail.com>
2021-06-12 23:49:58 +0000

    Fixed build process

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-import MBlockquote from "./Blockquote.vue";
-import MButton from "./Button.vue";
-import MDl from "./Dl.vue";
-import MOl from "./Ol.vue";
-import MTable from "./Table.vue";
-import MUl from "./Ul.vue";
-
-export { MBlockquote, MButton, MDl, MOl, MTable, MUl };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export * from "./components/index";
+import MBlockquote from "./components/Blockquote.vue";
+import MButton from "./components/Button.vue";
+import MDl from "./components/Dl.vue";
+import MOl from "./components/Ol.vue";
+import MTable from "./components/Table.vue";
+import MUl from "./components/Ul.vue";
+
+export { MBlockquote, MButton, MDl, MOl, MTable, MUl };
